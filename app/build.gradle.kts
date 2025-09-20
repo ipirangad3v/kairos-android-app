b129b2571b9e966a31c0f799993d09ca367d5e0f
Anthoni Ipiranga <anthoni.ipiranga@gmail.com>
2025-09-19 21:51:20 -0300

    bump versionCode to 5 and versionName to 1.2.2 for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "digital.tonima.kairos"
         minSdk = 28
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.2.1"
+        versionCode = 5
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
