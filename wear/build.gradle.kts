f8e28515b99db0385bba9e5adb453b1734cf1b5a
Anthoni Ipiranga <anthoni.ipiranga@gmail.com>
2025-09-26 02:35:05 -0300

    feat: increment version code for Kairos Wear app

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "digital.tonima.kairos"
         minSdk = rootProject.extra["MIN_SDK_VERSION"].toString().toInt()
         targetSdk = rootProject.extra["TARGET_SDK_VERSION"].toString().toInt()
-        versionCode = rootProject.extra["APP_VERSION_CODE"].toString().toInt()
+        versionCode = rootProject.extra["APP_VERSION_CODE"].toString().toInt() + 1
         versionName = (rootProject.extra["APP_VERSION_NAME"]).toString() + "-wear"
     }
 
